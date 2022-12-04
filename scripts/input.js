d2eb4a928dcd7d13c800a9cd9fc4559953951317
Styro457 <raduursache6@gmail.com>
2022-12-04 15:37:16 +0200

    Improved UI

--- scripts/input.js
+++ scripts/input.js
@@ -1,6 +1,7 @@
-function onFormSubmit(word) {
+function onFormSubmit(word, button) {
     const createResults = async () => {
         const result = await addResultsForKeyword(word);
     }
     createResults().then(r => console.log("DONE"));
+    button.disabled = true;
 }
