65d07ec436be09dc33c3df56100bcbfd44b20b28
Styro457 <raduursache6@gmail.com>
2022-12-04 15:37:11 +0200

    Replaced google-ngram api functionality with datamuse api

--- scripts/apis/datamuse.js
+++ scripts/apis/datamuse.js
@@ -1,11 +1,30 @@
-function getRelatedWords(word, firstLetter, lastLetter, limit) {
+function getRelatedWords(word, topics, firstLetter, lastLetter, limit, frequencyLimit, minLenght) {
     return new Promise((resolve) => {
-        fetch("https://api.datamuse.com/words?ml=" + word + "&max=" + limit).then(response => {
+        let url = "https://api.datamuse.com/words?ml=" + word + "&max=" + limit + "&md=df";
+        if(topics !== null && topics.length > 0) {
+            let topicsString = topics[0];
+            for (let i = 1; i < Math.max(topics.length, 5); i++) {
+                topicsString = topicsString + "," + topics[i];
+            }
+            url = url + "&topics=" + topicsString;
+        }
+        fetch("https://api.datamuse.com/words?ml=" + word + "&max=" + limit + "&md=df").then(response => {
             return response.json()
         }).then(data => {
             const words = []
+            let frequency, word;
             for(let i in data) {
-                words.push(data[i]["word"])
+                frequency = data[i]["tags"][data[i]["tags"].length-1]
+                word = data[i]["word"];
+                addWord(word);
+                if(frequency.startsWith(frequencyLimit) && !word.includes(" ") && word.length >= minLenght) {
+                    words.push(
+                        {
+                            word: word,
+                            definitions: data[i]["defs"],
+                            frequency: frequency
+                        });
+                }
             }
             resolve(words)
         })
--- scripts/generator.js
+++ scripts/generator.js
@@ -1,37 +1,74 @@
 const resultsDiv = document.getElementById("results");
+const allWordsText = document.getElementById("allWordsText");
 
-function addResult(word, frequency) {
+let globalWords = [];
+
+
+function addResult(word) {
     console.log("RESULT");
     let div = document.createElement("div");
     div.className = "result";
 
     let text = document.createElement("p");
     text.className = "resultText";
-    text.textContent = word + " : " + frequency;
+    text.textContent = word.word;
+
+    let description = document.createElement("p");
+    description.className = "resultDescription";
+    let definitions = "";
+    if(word.definitions !== undefined) {
+        for (let i = 0; i < word.definitions.length; i++) {
+            definitions = definitions + "(" + word.definitions[i].replace(/\s/, ".) ") + "\r\n\r\n";
+        }
+    }
+    description.textContent = definitions;
     div.appendChild(text);
+    div.appendChild(description);
 
     resultsDiv.appendChild(div);
 }
 
-async function addResultsForKeyword(keyword) {
-    getRelatedWords(keyword, undefined, undefined, 300).then(words => {
-        console.log(words);
-        for(let i = 1; i < words.length; i++) {
-            if(words[i].includes(' '))
-                continue;
-            (function (i) {
-                setTimeout(function () {
-                    console.log("WORD: " + words[i]);
-                    getAverageSearches(words[i]).then(frequency => {
-                        console.log(words[i] + " : " + frequency);
-                        if(frequency < 0.0000002) {
-                            addResult(words[i], frequency);
-                        }
-                    });
-                }, 800 * i);
-            })(i);
-        }
+function addWord(word) {
+    allWordsText.textContent = allWordsText.textContent + word + "\r\n";
+}
+
+function checkWords(i) {
+    addResult(globalWords[i]);
+    setTimeout(checkWords.bind(null, i + 1), 50);
+}
+
+async function addResultsForKeyword(keywordsRaw) {
+    const keywords = keywordsRaw.split(/[\s,]+/);;
+    console.log(keywords);
+    for(let i = 0; i < keywords.length; i++) {
+        await getRelatedWords(
+            keywords[i],
+            keywords.slice().splice(i, 1),
+            undefined, undefined,
+            300, "f:0.0", 4).then(words => {
+            globalWords = globalWords.concat(words);
+        });
+    }
+    for(let i = 0; i < keywords.length; i++) {
+        await getRelatedWords(
+            keywords[i],
+            null,
+            undefined, undefined,
+            300, "f:0.0", 4).then(words => {
+            globalWords = globalWords.concat(words);
+        });
+    }
+    console.log(globalWords);
+    const seen = {};
+    globalWords = globalWords.filter(function(item) {
+        let k = item.word;
+        return seen.hasOwnProperty(k) ? false : (seen[k] = true);
+    })
+    globalWords = globalWords.sort(function(a, b){
+        return a.word.length - b.word.length;
     });
+    console.log(globalWords);
+    checkWords(0);
 }
 
 //addResultsForKeyword("darkness");
