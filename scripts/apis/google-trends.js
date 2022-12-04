96791640a2834bc9083000c47b9123adf25ddfe0
Styro457 <raduursache6@gmail.com>
2022-12-04 20:52:50 +0200

    Removed google-trends api handler

--- scripts/apis/google-trends.js
+++
@@ -1,9 +0,0 @@
-function getAverageSearches(word) {
-    return new Promise((resolve) => {
-        fetch("https://google-trends-average.herokuapp.com/average-word-search/" + word).then(response => {
-            return response.json();
-        }).then(data => {
-            resolve(data);
-        })
-    })
-}
