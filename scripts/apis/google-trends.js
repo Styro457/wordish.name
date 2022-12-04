496dd1a53337c9fe3d2fc150adf44c793a65a9e2
Styro457 <raduursache6@gmail.com>
2022-12-04 15:36:40 +0200

    Fixed syntax

--- scripts/apis/google-trends.js
+++ scripts/apis/google-trends.js
@@ -1,7 +1,7 @@
 function getAverageSearches(word) {
     return new Promise((resolve) => {
         fetch("https://google-trends-average.herokuapp.com/average-word-search/" + word).then(response => {
-            return response.json()
+            return response.json();
         }).then(data => {
             resolve(data);
         })
